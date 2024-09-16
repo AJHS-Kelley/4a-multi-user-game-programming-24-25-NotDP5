31e3150e56df744de2feb72d3d7fdd2e869a9cb0
NotDP5 <dmpond@icloud.com>
2024-09-16 14:51:57 -0400

    555

--- 01_gaming_exercises/04_rock_paper_scissors/RockPaperScissors.cs
+++ 01_gaming_exercises/04_rock_paper_scissors/RockPaperScissors.cs
@@ -2,5 +2,27 @@ using System;
 class Template {
   static void Main() {
     
+
+int playerScore = 0;
+int cpuScore = 0;
+int playerChoice;
+string cpuChoice;
+string guess; 
+
+string 
+
+while 
+
+
+
+
+
+
+
+
+
+
+
+
   }
 }
