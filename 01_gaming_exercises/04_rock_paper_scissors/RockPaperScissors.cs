23a81f425c86d694ff491acc2925bbbe1afa0c53
NotDP5 <dmpond@icloud.com>
2024-09-24 13:59:53 -0400

    Update RockPaperScissors.cs

--- 01_gaming_exercises/04_rock_paper_scissors/RockPaperScissors.cs
+++ 01_gaming_exercises/04_rock_paper_scissors/RockPaperScissors.cs
@@ -15,16 +15,20 @@ string cpuChoice = "";
 Console.WriteLine ("Welcome to my Rock, Paper, Scissors Bot!");
 Console.WriteLine ("Please enter your name and hit enter.");
 playerName = Console.ReadLine();
+Console.WriteLine (" ");
 Console.WriteLine ($"Hello {playerName}. Is that correct?");
 Console.WriteLine ("Please type yes or no, then press enter.");
+Console.WriteLine (" ");
 string isCorrect = Console.ReadLine().ToLower();
 
 if (isCorrect == "yes")
 {
+  Console.WriteLine (" ");
   Console.WriteLine ($"Great! I'll call you {playerName}.");
 }
 else if (isCorrect == "no")
 {
+  Console.WriteLine (" ");
   Console.WriteLine ("Please enter your name and hit enter.");
   playerName = Console.ReadLine();
 }
@@ -33,14 +37,16 @@ else
   Console.WriteLine ($"Unable to process your request. Please truy again.");
 }
 
-Console.WriteLine ("                                ");
+Console.WriteLine (" ");
 Console.WriteLine ("Rules for this bot:");
 Console.WriteLine ("You will choose Rock, Paper, or Scissors and the CPU will do the same at random.");
 Console.WriteLine ("A winner will be decided by the regular rock paper scissors rules:");
 Console.WriteLine ("Rock beats Scissors. Scissors beats Paper. Paper beats Rock.");
+Console.WriteLine (" ");
 
 Console.WriteLine ($"Do these rules make sense to you {playerName}? Select yes or no and hit enter.");
 string isCorrect2 = Console.ReadLine().ToLower();
+Console.WriteLine (" ");
 
 if (isCorrect2 == "yes")
 {
@@ -52,6 +58,23 @@ else
   playerName = Console.ReadLine();
 }
 
+Console.WriteLine (" ");
+
+Console.WriteLine ($"Are you ready to start the game {playerName}? Select yes or no and hit enter.");
+string isCorrect3 = Console.ReadLine().ToLower();
+
+if (isCorrect3 == "yes")
+{
+  Console.WriteLine ("Ok let's start!");
+}
+else
+{
+  Console.WriteLine ($"Oh well {playerName}, the game is starting! Hit enter to continue.");
+  playerName = Console.ReadLine();
+}
+
+
+Console.WriteLine (" ");
 while (playerScore < 5 && cpuScore < 5)
 {
   //Print the scores.
