c2e57ee60a1c0ded332a808caef842f083a2c3ee
NotDP5 <dmpond@icloud.com>
2024-09-30 14:46:22 -0400

    RPS
    Will need to be fixed.

--- 01_gaming_exercises/04_rock_paper_scissors/RPS_Performance.cs
+++ 01_gaming_exercises/04_rock_paper_scissors/RPS_Performance.cs
@@ -1,62 +1,51 @@
 using System;
-class RPS {
+class Template {
   static void Main() {
    
 
-// Player Variables
-int playerScore = 0;
-string playerChoice = "";
-int numDraws = 0;
+    // Player Variables
+  int playerScore = 0;
+  string playerChoice = "";
+  int numDraws = 0;
 
-// CPU Variables
-int cpuScore = 0;
-string cpuChoice = "";
+  // CPU Variables
+  int cpuScore = 0;
+  string cpuChoice = "";
 
-Console.WriteLine ("Welcome to my Rock, Paper, Scissors Bot!");
-Console.WriteLine ("Please enter your name and hit enter.");
-playerName = Console.ReadLine();
+  Console.WriteLine ("Welcome to my Rock, Paper, Scissors Bot!");
 
-// Testing game loop
-int loopCount = 0;
-int loopsReqs = 0; // Regs / Req is universal abbrav. for Request / Requests
-Console.WriteLine ("How many loops do you need? Type an integer and hit enter.");
-loopsReqs = Convert.ToInt32(Console.ReadLine());
+  // Testing game loop
+  int loopCount = 0;
+  int loopReqs = 0; // Regs / Req is universal abbrav. for Request / Requests
+  Console.WriteLine ("How many loops do you need? Type an integer and hit enter.");
+  loopReqs = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine (" ");
-while (playerScore < 5 && cpuScore < 5)
-{
-  //Print the scores.
-  Console.WriteLine ($"Your Score: {playerScore} CPU Score: {cpuScore}");
   Console.WriteLine (" ");
+  while (loopCount < loopReqs)
+  {
+    Random rnd = new Random();
+    int cpuRand = rnd.Next(0,2);
+    if (cpuRand == 0)
+    {
+      cpuChoice = "rock";
+    }
+    else if (cpuRand == 1)
+    {
+      cpuChoice = "paper";
+    }
+    else if (cpuRand == 2)
+    {
+      cpuChoice = "scissors";
+    }
+    else
+    {
+     Console.WriteLine ("Unable to determine a CPU choice.");
+    }
 
-  // Allow the player to select Rock, Paper, or Scissors.
-  Console.WriteLine ("Please choose rock, paper, or scissors.");
-  playerChoice = Console.ReadLine().ToLower();
-  if (playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors")
-  {
-  Console.WriteLine ("Please choose rock, paper, or scissors.");
-  playerChoice = Console.ReadLine().ToLower();
   }
 
-  // Allow CPU to select randomly.
-  Random rnd = new Random();
-  int cpuRand = rnd.Next(0,2);
-  if (cpuRand == 0)
-  {
-    cpuChoice = "rock";
-  }
-  else if (cpuRand == 1)
-  {
-    cpuChoice = "paper";
-  }
-  else if (cpuRand == 2)
-  {
-    cpuChoice = "scissors";
-  }
-  else
-  {
-    Console.WriteLine ("Unable to determine a CPU choice.");
-  }
+
+
   Console.WriteLine (" ");
   Console.WriteLine ($"CPU choice {cpuChoice}");
   // Compare the choices and determine a winner.
@@ -78,6 +67,7 @@ while (playerScore < 5 && cpuScore < 5)
     Console.WriteLine (" ");
     Console.WriteLine ($"You chose {playerChoice} and the CPU chose {cpuChoice}");
     Console.WriteLine ("You and the CPU tied.");
+    numDraws++;
   }
   else if (playerChoice == "paper" && cpuChoice == "rock")
   {
@@ -98,6 +88,7 @@ while (playerScore < 5 && cpuScore < 5)
     Console.WriteLine (" ");
     Console.WriteLine ($"You chose {playerChoice} and the CPU chose {cpuChoice}");
     Console.WriteLine ("You and the CPU tied.");
+    numDraws++;
   }
     else if (playerChoice == "scissors" && cpuChoice == "rock")
   {
@@ -118,13 +109,17 @@ while (playerScore < 5 && cpuScore < 5)
     Console.WriteLine (" ");
     Console.WriteLine ($"You chose {playerChoice} and the CPU chose {cpuChoice}");
     Console.WriteLine ("You and the CPU tied.");
+    numDraws++;
   }
   }
+	loopCount++
   // Output the results to the screen.
   // Give the winner a point.
+  loopCount++;
+  
 }
 
+Console.WriteLine ($"Player Score: {playerScore}");
+Console.WriteLine ($"CPU Score: {cpuScore}");
+Console.WriteLine ($"Draws: {numDraws}");
 
-
-
-  }
