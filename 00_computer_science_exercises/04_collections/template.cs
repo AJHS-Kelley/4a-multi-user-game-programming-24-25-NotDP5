3332b73e079ec85b47226c82ba1c954f7ba95c51
NotDP5 <dmpond@icloud.com>
2024-11-07 14:32:14 -0500

    Update template.cs

--- 00_computer_science_exercises/04_collections/template.cs
+++ 00_computer_science_exercises/04_collections/template.cs
@@ -1,82 +1,95 @@
-// // // Collections Example, David Ponder, v0.0
-// // using System;
-// // using System.Linq;
-// // using System.Collections;
+// Collections Example, David Ponder, v0.0
+// using System;
+// using System.Linq;
+// using System.Collections;
 
 // // class Collections {
 // //   static void Main() { // Do not delete
     
-// //     // /*
-// //     // -- In C#, a collection is known as as array.
-// //     // -- Each item in an array is known as an element. 
-// //     // -- Number of elements in an array can not be changed
-// //     // -- Contents in each element can chnage. 
-// //     // -- Arrays are ordered, meaning each item has a fixed position. 
-// //     // -- The position of the element is the index. 
-// //     // -- The first element index is a index. 
-// //     // */
+// /*
+// -- In C#, a collection is known as as array.
+// -- Each item in an array is known as an element. 
+// -- Number of elements in an array can not be changed
+// -- Contents in each element can chnage. 
+// -- Arrays are ordered, meaning each item has a fixed position. 
+// -- The position of the element is the index. 
+// -- The first element index is a index. 
+// */
 
-// //     // // Declaring and defining an array
-// //     // string [] breakfastFoods = {"Pancakes", "Eggs", "Waffle", "Bacon", "Cereal"};
-// //     // int [] testScores = {0, 25, 15, 75, 99};
-// //     // double[] GPA = {0.27D, 1.99D, 4.02D, 0.0D, 3.25D};
+// // Declaring and defining an array
+// string [] breakfastFoods = {"Pancakes", "Eggs", "Waffle", "Bacon", "Cereal"};
+// int [] testScores = {0, 25, 15, 75, 99};
+// double[] GPA = {0.27D, 1.99D, 4.02D, 0.0D, 3.25D};
 
-// //     // // Print Array Content -- All Elements on Single Line 
-// //     // Console.WriteLine ("The elements of each array are:");
-// //     // Console.WriteLine (" ");
-// //     // Console.WriteLine ("breakFastFoods: \n" + String.Join("\n", breakfastFoods));
-// //     // Console.WriteLine (); // Print an empty line to the screen. 
+// // Print Array Content -- All Elements on Single Line 
+// Console.WriteLine ("The elements of each array are:");
+// Console.WriteLine (" ");
+// Console.WriteLine ("breakFastFoods: \n" + String.Join("\n", breakfastFoods));
+// Console.WriteLine (); // Print an empty line to the screen. 
     
-// //     // Console.WriteLine ("The elements of each array are:");
-// //     // Console.WriteLine (" ");
-// //     // Console.WriteLine ("testScores: \n" + String.Join("\n", testScores));
-// //     // Console.WriteLine (); // Print an empty line to the screen. 
+// Console.WriteLine ("The elements of each array are:");
+// Console.WriteLine (" ");
+// Console.WriteLine ("testScores: \n" + String.Join("\n", testScores));
+// Console.WriteLine (); // Print an empty line to the screen. 
     
-// //     // Console.WriteLine ("The elements of each array are:");
-// //     // Console.WriteLine (" ");
-// //     // Console.WriteLine ("GPA: \n" + String.Join("\n", GPA));
-// //     // Console.WriteLine (); // Print an empty line to the screen. 
+// Console.WriteLine ("The elements of each array are:");
+// Console.WriteLine (" ");
+// Console.WriteLine ("GPA: \n" + String.Join("\n", GPA));
+// Console.WriteLine (); // Print an empty line to the screen. 
 
-// //     // // How long is my array? Determing array length
-// //     // Console.WriteLine ("The length of the breakFastFoods array is: " + breakfastFoods.Length);
-// //     // Console.WriteLine ("The length of the testScores array is: " + testScores.Length);
-// //     // Console.WriteLine ("The length of the GPA array is: " + GPA.Length);
+// How long is my array? Determing array length
+// Console.WriteLine ("The length of the breakFastFoods array is: " + breakfastFoods.Length);
+// Console.WriteLine ("The length of the testScores array is: " + testScores.Length);
+// Console.WriteLine ("The length of the GPA array is: " + GPA.Length);
 
-// //     // Console.WriteLine (" ");
-// //     // // .Length is known as a property of the array. 
-// //     // // Accessing elements in the array with the index.
-// //     // Console.WriteLine ("The first element in breakFast is: " + breakfastFoods[0]);
-// //     // Console.WriteLine ("The first element in testScores is: " + testScores[3]);
-// //     // Console.WriteLine ("The first element in GPA is: " + breakfastFoods[4]);
+// Console.WriteLine (" ");
+// // .Length is known as a property of the array. 
+// // Accessing elements in the array with the index.
+// Console.WriteLine ("The first element in breakFast is: " + breakfastFoods[0]);
+// Console.WriteLine ("The first element in testScores is: " + testScores[3]);
+// Console.WriteLine ("The first element in GPA is: " + breakfastFoods[4]);
     
-// //     // Console.WriteLine (" ");
+// Console.WriteLine (" ");
 
-// //     // // Shortcut to access the last item in an array
-// //     // Console.WriteLine ("the last element of breakFastFoods is: " + breakfastFoods[breakfastFoods.Length - 1]);
-// //     // Console.WriteLine ("the last element of testScores is: " + testScores[testScores.Length - 1]);
-// //     // Console.WriteLine ("the last element of GPA is: " + GPA[GPA.Length - 1]);
+// // Shortcut to access the last item in an array
+// Console.WriteLine ("the last element of breakFastFoods is: " + breakfastFoods[breakfastFoods.Length - 1]);
+// Console.WriteLine ("the last element of testScores is: " + testScores[testScores.Length - 1]);
+// Console.WriteLine ("the last element of GPA is: " + GPA[GPA.Length - 1]);
     
-// //     // // Changing elements in an array
-// //     // breakfastFoods[2] = "Green Eggs and Ham";
-// //     // testScores[0] = 56;
-// //     // GPA[3] = 4.2D;
-// //     // Console.WriteLine ("The elements of each array are:");
-// //     // Console.WriteLine (" ");
-// //     // Console.WriteLine ("breakFastFoods: \n" + String.Join("\n", breakfastFoods));
-// //     // Console.WriteLine (); // Print an empty line to the screen. 
+// // Changing elements in an array
+// breakfastFoods[2] = "Green Eggs and Ham";
+// testScores[0] = 56;
+// GPA[3] = 4.2D;
+// Console.WriteLine ("The elements of each array are:");
+// Console.WriteLine (" ");
+// Console.WriteLine ("breakFastFoods: \n" + String.Join("\n", breakfastFoods));
+// Console.WriteLine (); // Print an empty line to the screen. 
     
 
-// //     // // Common errors with arrays
-// //     // // Console.WriteLine (breakfastFoods[99]); // Index out of bounds -- According index that does not exist in the array
+// // Common errors with arrays
+// // Console.WriteLine (breakfastFoods[99]); // Index out of bounds -- According index that does not exist in the array
     
-// //     // /*
-// //     // testScores[0] = "99"; // Error message "Cannot implicitly convert dataType to dataType"
-// //     // Console.WriteLine ("testScores: \n" + String.Join("\n", testScores));
-// //     // Console.WriteLine (); // Print an empty line to the screen. 
-// //     // */
+// /*
+// testScores[0] = "99"; // Error message "Cannot implicitly convert dataType to dataType"
+// Console.WriteLine ("testScores: \n" + String.Join("\n", testScores));
+// Console.WriteLine (); // Print an empty line to the screen. 
+// */
 
-// //     // // Sorting an array
-// //     // int[] intArr = []
+// // Sorting an array
+// int[] intArr = {-25, 50, 68, 523, -1205, 675483, -1, 55, 0, 124};
+// string[] zooAnimals = {"Rhinos", "Zebra", "Elephants", "Moose", "Iguana"};
+Array.Sort(intArr);
+Array.Sore(zooAnimals);
+
+// Array.Sort will sort string arrays alphabetically A-Z and numerical arrays from least to greatest
+Console.WriteLine("The elements of each array are:");
+Console.WriteLine("zooAnimals after Array.Sort(): \n" + String.Join(", ", zooAnimals));
+Console.WriteLine(); // Print an empty line to the screen. 
+
+//Finding the Min, Max, and Sum, fro numerical arrays
+Console.WriteLine("The minimum value for intArr is: " + intArr.Min());
+Console.WriteLine("The minimum value for intArr is: " + intArr.Max());
+Console.WriteLine("The minimum value for intArr is: " + intArr.Sum());
 
 
 
