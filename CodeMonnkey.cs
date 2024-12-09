4df4fcab781ff177f09afd89a013a675f0217d25
NotDP5 <dmpond@icloud.com>
2024-12-09 09:42:40 -0500

    Update CodeMonnkey.cs

--- CodeMonnkey.cs
+++ CodeMonnkey.cs
@@ -2,6 +2,8 @@ using System;
 class Program {
   static void Main(string[] args) {
 
+// First Video
+
 // Functions
 
     Console.WriteLine("Hello New World");
@@ -50,9 +52,82 @@ class Program {
     // Comments -- Two forward slashes (" // ") or " /* " at the beginning of the comment(s) and " */ " at the end of the comment(s).
     // The program will ignore any code/messages you comment out.
     // Comments can help other programs who are not working on the code know what everything is doing and why. 
+
+    // ---------------------------------------------------------------------------------------------------------------------------------
+    // ---------------------------------------------------------------------------------------------------------------------------------
+
+    // Loops -- You have 4 types of loops: while, do while, for, and foreach.
+    // Can help you run certain logics at one time.
+
+    // While loops -- A simple loop that will run the condition thats with the {} as long as that condition is true. 
+    static void Main(string[] args) {
+      while (true) {
+        Console.WriteLine("Do Something")
+      }
+    } // This code will print forever since the condition will stay true. 
+
+      static void Main(string[] args) {
+      int i = 0;
+      while (i < 3) {
+        Console.WriteLine("Do Something")
+      }
+    } // This code will print until it reaches the number 2 since 3 is not less than 3. 
+
+    // ---------------------------------------------------------------------------------------------------------------------------------
+    // Do loops -- Runs the condition that is stated and will check afterwards compared to the while loop checking before the condition runs.  
+
+    do {
+      Console.WriteLine("Do while loop " + i);
+      i++;
+    } while (i < 3); 
+
+    // ---------------------------------------------------------------------------------------------------------------------------------
+    // For loops -- Broken up into 3 parts: the initialzer, the condition, and the incrementer. 
+    // Makes it easier to handle the in 
+
+    for (int i = 0; i < 3; i++) { // It will check in chronological order. Starting with the initialzer, then the condition, and then the incrementer. 
+      Console.WriteLine("For Loop " + i);
+    }
+
+    // For loops can be used to cycle through an array.
+
+    string[] nameArray = new string[] {
+      "David"
+      "Todd"
+      "Mike"
+      "Ronald"
+    };
+    
+    for (int i = 0; i < nameArray.Length; i++) {  // This will bring out the entire list of nameArray. 
+      Console.WriteLine(nameArray[i]);
+    }
+
+    // ---------------------------------------------------------------------------------------------------------------------------------
+    // foreach loops -- Uses an iterator instead of an index to the collection element.
+    foreach (string name in nameArray) {
+      Console.WriteLine(name);
+    }
+
+    // ---------------------------------------------------------------------------------------------------------------------------------
+    // ---------------------------------------------------------------------------------------------------------------------------------
+
+    // Classes -- One of the core coding concepts that is basically a type 
+    // Can make a type and can create instances or object that go with that type. 
+
+    static void Main(string[] args) {
+      int age = 54;
+      Player David = new Player();
+      Player Preston = new Player();
+      Console.WriteLine(player);
+    }
+
+    class Player {
+      Player() {
+        
+      }
+    }
     
 
-   
 
 
 
